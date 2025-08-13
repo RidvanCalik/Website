3bfe947e97dc9af65f8a602e0f890d7497e8fd96
RidvanCalik <calikridvan37@outlook.com.tr>
2025-08-13 15:49:09 +0300

    images optimization & added responsibility

--- src/components/mainpage/section3.jsx
+++ src/components/mainpage/section3.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import TypeIt from "typeit-react";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, FreeMode } from 'swiper/modules';
+import { Autoplay, FreeMode, Mousewheel } from 'swiper/modules';
 
 
 import 'swiper/css/navigation';
@@ -15,21 +15,23 @@ function Section3() {
         <section id='section3'>
             <h1>Blog</h1>
             <Swiper
-                cssMode={true}
-
-                slidesPerView={4}
-                freeMode={true}
+                modules={[Mousewheel]}
                 loop={true}
+                spaceBetween={10}
+                slidesPerView={2.5}
+                mousewheel={{
+                    forceToAxis: true,
+                    thresholdDelta: 30
+                }}
 
-                modules={[FreeMode]}
 
             >
-                <SwiperSlide className='card'><a href='https://www.google.com'><img src="/img/blog.jpg" alt="" /><div className='cardBody'><h3>En iyi Korku Filmleri</h3><p>Şahsıma Münasır Korku filmleri</p></div></a></SwiperSlide>
-                <SwiperSlide className='card'><a href='https://www.google.com'><img src="/img/blog2.jpg" alt="" /><div className='cardBody'><h3>Gökçeada Tatil</h3><p>Türkiyenin en büyük adası olma özelliğini taşıyor</p></div></a></SwiperSlide>
-                <SwiperSlide className='card'><a href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
-                <SwiperSlide className='card'><a href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
-                <SwiperSlide className='card'><a href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
-
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog.jpg" alt="" /><div className='cardBody'><h3>En iyi Korku Filmleri</h3><p>Şahsıma Münasır Korku filmleri</p></div></a></SwiperSlide>
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog2.jpg" alt="" /><div className='cardBody'><h3>Gökçeada Tatil</h3><p>Türkiyenin en büyük adası olma özelliğini taşıyor</p></div></a></SwiperSlide>
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
+                <SwiperSlide className='card'><a target='_blank' href='https://www.google.com'><img src="/img/blog3.jpg" alt="" /><div className='cardBody'><h3>Fotoğraf Köşesi</h3><p>Amatör fotoğraflarım</p></div></a></SwiperSlide>
             </Swiper>
         </section>
 
